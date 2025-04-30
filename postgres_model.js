5a054a45814fe96f01a8c47ae12a6cf75bf92343
nick1987 <nikunj.patriwala@gmail.com>
2025-04-30 12:22:26 +0530

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -4,3 +4,10 @@ var pg_host="gitguardians.com:9082/BLUDB";
 var pg_user="root";
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+
+var pg_port_prod=5678;
+var pg_host_prod="gitguardians.com:9081/BLUDB";
+var pg_user_prod="root";
+
+var mongo_uri_prod="mongodb+srv://root:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
